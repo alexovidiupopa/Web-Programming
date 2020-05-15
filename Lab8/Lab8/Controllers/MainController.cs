2ebdaee96952a88cd863bbec53f2ae149a5aaf2e
Alex-Ovidiu Popa <popaalexovidiu@gmail.com>
2020-05-15 12:47:59 +0300

    started working on asp lab

---
+++ Lab8/Lab8/Controllers/MainController.cs
@@ -0,0 +1,68 @@
+﻿using Lab8.Data_Abstraction_Layer;
+using Lab8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab8.Controllers
+{
+    public class MainController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("GetReports");
+        }
+
+        public string TestController()
+        {
+            return "Testing the MainController .. OK!";
+        }
+
+
+
+        public ActionResult GetReports()
+        {
+            DAL dal = new DAL();
+            List<LogReport> slist = dal.GetLogReports();
+            ViewData["reports"] = slist;
+            return View("ViewGetReports");
+        }
+
+        public ActionResult AddStudent()
+        {
+            return View("AddNewStudent");
+        }
+
+        public ActionResult SaveReport()
+        {
+            LogReport log = new LogReport();
+            log.User_id = int.Parse(Request.Params["user_id"]);
+            log.Date = Request.Params["date"];
+            log.Severity = Request.Params["severity"];
+            log.Type = Request.Params["type"];
+            DAL dal = new DAL();
+            dal.AddLogReport(log);
+            return RedirectToAction("GetReports");
+        }
+
+        public string GetReportsOfType()
+        {
+            string type =(Request.Params["type"]);
+            DAL dal = new DAL();
+            List<LogReport> slist = dal.GetReportsOfType(type);
+            ViewData["reports"] = slist;
+
+            string result = "<table><thead><th>Id</th><th>User_id</th><th>Type</th><th>Severity</th></thead>";
+
+            foreach (LogReport stud in slist)
+            {
+                result += "<tr><td>" + stud.Id + "</td><td>" + stud.User_id + "</td><td>" + stud.Type + "</td><td>" + stud.Severity + "</td><td></tr>";
+            }
+
+            result += "</table>";
+            return result;
+        }
+    }
+}
---
+++ Lab8/Lab8/Data Abstraction Layer/DAL.cs
@@ -0,0 +1,144 @@
+﻿using Lab8.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab8.Data_Abstraction_Layer
+{
+    public class DAL
+    {
+
+        public MySqlConnection getConnection()
+        {
+            string myConnectionString;
+            myConnectionString = "server=127.0.0.1;uid=alex;pwd=alex;database=logreports;";
+            return new MySqlConnection(myConnectionString);
+        
+        } 
+
+        public List<LogReport> GetLogReports()
+        {         
+            List<LogReport> slist = new List<LogReport>();
+            try
+            {
+                MySqlConnection conn = getConnection();
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from LogReport" ;
+                MySqlDataReader myreader = cmd.ExecuteReader();
+
+                while (myreader.Read())
+                {
+                    LogReport report = new LogReport();
+                    report.Id = myreader.GetInt32("id");
+                    report.User_id = myreader.GetInt32("user_id");
+                    report.Type = myreader.GetString("type");
+                    report.Severity = myreader.GetString("severity");
+                    report.Date = myreader.GetString("date");
+                    slist.Add(report);
+                }
+                myreader.Close();
+
+                conn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return slist;
+        }
+
+        public void AddLogReport(LogReport log)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            MySqlConnection conn = getConnection();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = "INSERT INTO LogReport(user_id, type, severity, date) VALUES (" +
+                log.User_id + ", '" + log.Type + "', '" + log.Severity + "', '" + log.Date + "');";  
+            cmd.ExecuteNonQuery();
+        }
+
+        public void DeleteLogReport(int id)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            MySqlConnection conn = getConnection();
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = "DELETE FROM LogReport WHERE id=" + id;
+            cmd.ExecuteNonQuery();
+        }
+
+        public List<LogReport> GetReportsOfType(string type)
+        {
+            List<LogReport> slist = new List<LogReport>();
+            try
+            {
+                MySqlConnection conn = getConnection();
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from LogReport where type='" + type + "'";
+                MySqlDataReader myreader = cmd.ExecuteReader();
+
+                while (myreader.Read())
+                {
+                    LogReport report = new LogReport();
+                    report.Id = myreader.GetInt32("id");
+                    report.User_id = myreader.GetInt32("user_id");
+                    report.Type = myreader.GetString("type");
+                    report.Severity = myreader.GetString("severity");
+                    report.Date = myreader.GetString("date");
+                    slist.Add(report);
+                }
+                myreader.Close();
+
+                conn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return slist;
+        }
+
+        public List<LogReport> GetReportsOfSeverity(string severity)
+        {
+            List<LogReport> slist = new List<LogReport>();
+            try
+            {
+                MySqlConnection conn = getConnection();
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from LogReport where severity='" + severity + "'";
+                MySqlDataReader myreader = cmd.ExecuteReader();
+
+                while (myreader.Read())
+                {
+                    LogReport report = new LogReport();
+                    report.Id = myreader.GetInt32("id");
+                    report.User_id = myreader.GetInt32("user_id");
+                    report.Type = myreader.GetString("type");
+                    report.Severity = myreader.GetString("severity");
+                    report.Date = myreader.GetString("date");
+                    slist.Add(report);
+                }
+                myreader.Close();
+
+                conn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return slist;
+        }
+    }
+}
